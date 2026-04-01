493b2880f323068d666a7b22a6f124815566d4a0
Maxim Astaoglo <70810340+NightlyZzz@users.noreply.github.com>
2026-04-01 18:45:13 +0300

    Update

--- routes/api.php
+++ routes/api.php
@@ -8,55 +8,55 @@ use App\Http\Controllers\Ticket\TicketLogController;
 use App\Http\Controllers\Ticket\TicketStatusController;
 use App\Http\Controllers\Ticket\TicketTypeController;
 use App\Http\Controllers\User\UserController;
-use App\Models\Ticket\Ticket;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->prefix('auth')->group(function (): void {
-    Route::post('/login', 'login')->name('auth.login');
-    Route::post('/register', 'register')->name('auth.register');
-    Route::post('/logout', 'logout')->name('auth.logout');
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
 });
 
-Route::controller(DepartmentController::class)->prefix('department')->group(function (): void {
-    Route::get('/all', 'showAll')->name('department.showAll');
+Route::prefix('public')->group(function () {
+    Route::get('/departments', [DepartmentController::class, 'showAll']);
+    Route::get('/roles', [RoleController::class, 'showAll']);
 });
 
-Route::controller(RoleController::class)->prefix('role')->group(function (): void {
-    Route::get('/all', 'showAll')->name('role.showAll');
-});
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::controller(UserController::class)->prefix('user')->group(function (): void {
-    Route::get('/', 'show')->name('user.show');
-    Route::get('/all', 'showAll')->name('user.show.all');
-    Route::get('/{user}', 'showById')->name('user.showById');
+    Route::controller(UserController::class)->prefix('user')->group(function (): void {
+        Route::get('/', 'show');
+        Route::get('/all', 'showAll');
+        Route::get('/{user}', 'showById');
 
-    Route::put('/', 'update')->name('user.update');
-    Route::put('/{user}', 'updateById')->name('user.update');
+        Route::put('/', 'update');
+        Route::put('/{user}', 'updateById');
 
-    Route::delete('/', 'destroy')->name('user.destroy');
-    Route::delete('/{user}', 'destroyById')->name('user.destroyById');
-});
+        Route::delete('/', 'destroy');
+        Route::delete('/{user}', 'destroyById');
+    });
 
-Route::controller(TicketController::class)->prefix('ticket')->group(function (): void {
-    Route::get('/my', 'showMy')->name('ticket.showMy');
-    Route::get('/all', 'showAll')->name('ticket.showAll');
-    Route::get('/{ticket}', 'show')->name('ticket.show');
+    Route::controller(TicketController::class)->prefix('ticket')->group(function (): void {
+        Route::get('/my', 'showMy');
+        Route::get('/all', 'showAll');
+        Route::get('/{ticket}', 'show');
 
-    Route::post('/', 'store')->name('ticket.store');
-    Route::post('/{ticket}/attach-log', 'attachLog')->name('ticket.attach');
+        Route::post('/', 'store');
+        Route::post('/{ticket}/attach-log', 'attachLog');
 
-    Route::put('/{ticket}', 'update')->name('ticket.update');
-});
+        Route::put('/{ticket}', 'update');
+    });
 
-Route::controller(TicketLogController::class)->prefix('ticket/log')->group(function (): void {
-    Route::get('/{ticket}', 'index')->name('ticket.log.show');
-    Route::post('/{ticket}', 'store')->name('ticket.log.store');
-});
+    Route::controller(TicketLogController::class)->prefix('ticket/log')->group(function (): void {
+        Route::get('/{ticket}', 'index');
+        Route::post('/{ticket}', 'store');
+    });
 
-Route::controller(TicketStatusController::class)->prefix('ticket/status')->group(function (): void {
-    Route::get('/all', 'all')->name('ticket.status.all');
-});
+    Route::controller(TicketStatusController::class)->prefix('ticket/status')->group(function (): void {
+        Route::get('/all', 'all');
+    });
 
-Route::controller(TicketTypeController::class)->prefix('ticket/type')->group(function (): void {
-    Route::get('/all', 'all')->name('ticket.type.all');
+    Route::controller(TicketTypeController::class)->prefix('ticket/type')->group(function (): void {
+        Route::get('/all', 'all');
+    });
+
 });
